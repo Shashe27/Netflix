e86aff74e8cfcc2c9fb09fc4fe48d398f6e0a221
Shashe27 <shashe.k2127@gmail.com>
2022-12-15 19:26:25 -0800

    Netflix

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import Banner from './components/Banner';
+import Row from './components/Row/Row';
+import requests from './requests';
+import Nav from "./components/Nav/Nav"
+function App() {
+  return (
+    <div className="App">
+     <Nav/>      
+     <Banner/>
+      <Row title="NETFLIX ORGINAL" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row
+        title="Documentaries Movies"
+        fetchUrl={requests.fetchDocumentaries}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/requests.jsx
@@ -0,0 +1,14 @@
+const API_KEY = "781414c71f74d4f041722a3f8d768629"
+
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+export default requests;
